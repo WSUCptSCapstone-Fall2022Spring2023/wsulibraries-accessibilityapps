fabd15d5d3eceedeeb0514a808bf1533e7be3ae9
Reagan <reagan.kelley@wsu.edu>
2022-10-14 00:24:09 -0700

    Added Image Extraction Test.

--- Code/PyPDF2/document_harvester.py
+++ Code/PyPDF2/document_harvester.py
@@ -15,9 +15,17 @@ def test_reader():
     text = page.extract_text()
     print(number_of_pages)
 
+def test_image_reader():
+    reader = PdfReader("example.pdf")
+    page = reader.pages[0]
+    count = 0
+    for image_file_object in page.images:
+        with open(str(count) + image_file_object.name, "wb") as fp:
+            fp.write(image_file_object.data)
+            count += 1
 
 def main():
-    test_reader()
+    test_image_reader()
 
 if __name__ == "__main__":
     main()
