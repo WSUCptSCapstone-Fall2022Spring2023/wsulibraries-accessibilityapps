1d0567dcc286725a7e2c510196ee7f7c9d10bb6d
Trent1048 <trentbultsma@gmail.com>
2022-10-19 10:11:11 -0700

    format extracted html

---
+++ sample/setup.bat
@@ -0,0 +1,2 @@
+pip install pdfminer.six
+pip install bs4
--- sample/src/pdf_extractor.py
+++ sample/src/pdf_extractor.py
@@ -5,22 +5,27 @@
 # Description: Extracts the data from a pdf as html.
 # ==================================================
 
-# ! Do we still want to use pdfminer? Currently using PyPDF2
-
 import os
+from bs4 import BeautifulSoup
 
-TEST_PDF = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/testInput.pdf"
-TEST_OUTPUT_HTML = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/testOutput.html"
+TEST_PDF = os.path.dirname(os.path.abspath(__file__)) + "/../../tests/testInput.pdf"
+TEST_OUTPUT_HTML = os.path.dirname(os.path.abspath(__file__)) + "/../../tests/testOutput.html"
 
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
 from pdfminer.layout import LAParams
 
+# read the pdf
 output_string = StringIO()
 with open(TEST_PDF, 'rb') as fin:
     extract_text_to_fp(fin, output_string, laparams=LAParams(),output_type='html', codec=None)
 
-with open(TEST_OUTPUT_HTML, 'w', encoding='utf-8') as output_file:
-    output_file.write(output_string.getvalue())
+# parse the html file
+parsedHtml = BeautifulSoup(output_string.getvalue(), 'html.parser')
 
-# TODO how do we get the images?
+# format the parsed html file
+formattedOutput = parsedHtml.prettify()
+
+# write to an html file
+with open(TEST_OUTPUT_HTML, 'w', encoding='utf-8') as output_file:
+    output_file.write(formattedOutput)
