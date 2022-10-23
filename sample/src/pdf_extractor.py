4f4e3cc8bd6a20799b9e645d157c4791c4d64a28
Trent1048 <trentbultsma@gmail.com>
2022-10-22 20:32:12 -0700

    split up the text by div and span

--- sample/src/pdf_extractor.py
+++ sample/src/pdf_extractor.py
@@ -26,6 +26,28 @@ parsedHtml = BeautifulSoup(output_string.getvalue(), 'html.parser')
 # format the parsed html file
 formattedOutput = parsedHtml.prettify()
 
+divTextSectionsAndFontSize = [] # TODO need to get the size of the font and make sure it is consistent in the div
+for div in parsedHtml.findAll('div'):
+    # to store a list of tuples of (text, font-style)
+    spanTextSectionsAndFont = []
+    for span in div.findAll('span'):
+        # get the text from the span without indentation or line 
+        # breaks or double spaces or trailing/leading whitespace
+        spanText = span.get_text().replace("\n", "").replace(
+            "\t", "").replace("  ", " ").strip()
+
+        # skip over empty spans
+        if spanText == "":
+            continue
+
+        # store the text and font style from the span
+        spanTextSectionsAndFont.append((spanText, span.attrs['style']))
+    print(spanTextSectionsAndFont)
+    
+    print()
+
 # write to an html file
+# (this will not be in the final product, but is 
+# helpful to look around in to see what is going on)
 with open(TEST_OUTPUT_HTML, 'w', encoding='utf-8') as output_file:
     output_file.write(formattedOutput)
