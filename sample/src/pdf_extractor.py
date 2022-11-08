f6fbb2741e05bcf892fdca71b02fa7714f1f649c
Trent Bultsma <trentbultsma@gmail.com>
2022-11-08 11:43:52 -0800

    properly implement italic and bold formatting

--- sample/src/pdf_extractor.py
+++ sample/src/pdf_extractor.py
@@ -7,6 +7,7 @@
 
 # imports
 import os
+from enum import Enum
 from io import StringIO
 from bs4 import BeautifulSoup
 from pdfminer.layout import LAParams
@@ -17,6 +18,58 @@ TEST_PDF = os.path.dirname(os.path.abspath(__file__)) + '/../../tests/testInput.
 TEST_OUTPUT_HTML = os.path.dirname(os.path.abspath(__file__)) + '/../../tests/testOutput.html'
 FONT_SIZE_STR = 'font-size'
 FONT_FAMILY_STR = 'font-family'
+
+class FontStyle(Enum):
+    '''Represents the style of text.'''
+    STANDARD = 1
+    BOLD = 2
+    ITALIC = 3
+    BOLD_ITALIC = 4
+    # TODO maybe add underline or strikethrough here
+
+def get_font_style_delimeter(style: FontStyle, is_start: bool) -> str:
+    '''Returns the html delimeter for a font style.
+    
+    Args:
+        style (FontStyle): The style of the font.
+        
+    Args:
+        is_start (bool): Whether the delimeter is for the start of the text or for the end.'''
+    
+    if style == FontStyle.STANDARD:
+        return ""
+    elif style == FontStyle.BOLD:
+        return "<" + ("" if is_start else "/") + "strong>"
+    elif style == FontStyle.ITALIC:
+        return "<" + ("" if is_start else "/") + "em>"
+    elif style == FontStyle.BOLD_ITALIC:
+        return get_font_style_delimeter(FontStyle.BOLD, is_start) + get_font_style_delimeter(FontStyle.ITALIC, is_start)
+
+def get_font_style(font_family_name: str) -> FontStyle:
+    '''Returns the font style of a font.
+
+    Args:
+        font_family_name (str): The name of the font.'''
+
+    bold = False
+    italic = False
+
+    # TODO update detection substrings
+    # Detect the font style.
+    if 'bold' in font_family_name.lower():
+        bold = True
+    if 'italic' in font_family_name.lower():
+        italic = True
+
+    # Return the font style detected.
+    if bold and italic:
+        return FontStyle.BOLD_ITALIC
+    elif bold:
+        return FontStyle.BOLD
+    elif italic:
+        return FontStyle.ITALIC
+    else:
+        return FontStyle.STANDARD
 
 # read the pdf
 pdf_content = StringIO()
@@ -45,7 +98,7 @@ def get_attributes(attribute_str: str) -> 'dict[str, str]':
         attribute_dict[name] = value
     return attribute_dict
 
-# will be formatted as a list[tuple(span info, text size)]
+# will be formatted as a list[tuple(span info, text size, font family)]
 div_text_info = []
 for div in parsed_html.findAll('div'):
     # keep track of the font sizes in the div, a list of tuples of (font size, text length using that size)
@@ -53,9 +106,11 @@ for div in parsed_html.findAll('div'):
     # size for all the spans, but sometimes there are little variations and in that case, the most
     # commonly appearing font size is used for a unified div. TODO may want to investigate this approach later
     font_size_distribution = []
+    # also keep track of the font to get the average font
+    font_family_distribution = []
 
-    # to store a list of tuples of (text, font-style)
-    span_text_sections_and_font = []
+    # to store a list of tuples of (text, font style)
+    span_text_sections_and_font_style = []
     for span in div.findAll('span'):
         # get the text from the span without indentation or line 
         # breaks or double spaces or trailing/leading whitespace
@@ -66,16 +121,21 @@ for div in parsed_html.findAll('div'):
         if span_text == '':
             continue
 
-        # store the text and font style from the span
         span_attributes = get_attributes(span.attrs['style'])
-        span_text_sections_and_font.append((span_text, span_attributes[FONT_FAMILY_STR]))
+        
+        # update the average font family
+        font_family = span_attributes[FONT_FAMILY_STR]
+        font_family_distribution.append((font_family, len(span_text)))
+
+        # store the text and font style from the span
+        span_text_sections_and_font_style.append((span_text, get_font_style(font_family)))
     
-        # update the font size
+        # update the average font size
         font_size = span_attributes[FONT_SIZE_STR]
         font_size_distribution.append((font_size, len(span_text)))
     
     # skip empty spans
-    if len(span_text_sections_and_font) == 0:
+    if len(span_text_sections_and_font_style) == 0:
         continue
 
     # calculate the most commonly appearing font size in the div
@@ -86,8 +146,16 @@ for div in parsed_html.findAll('div'):
         font_size_distribution_dict[font_size] += text_len
     div_font_size = sorted(font_size_distribution_dict.items(), key=lambda sizeLenTup : sizeLenTup[1], reverse=True)[0][0]
 
+    # calculate the most commonly appearing font family in the div
+    font_family_distribution_dict = {}
+    for font_family, text_len in font_family_distribution:
+        if font_family not in font_family_distribution_dict.keys():
+            font_family_distribution_dict[font_family] = 0
+        font_family_distribution_dict[font_family] += text_len
+    div_font_family = sorted(font_family_distribution_dict.items(), key=lambda sizeLenTup : sizeLenTup[1], reverse=True)[0][0]
+
     # add the span data to the div info
-    div_text_info.append((span_text_sections_and_font, div_font_size))
+    div_text_info.append((span_text_sections_and_font_style, div_font_size, div_font_family))
 
 # start the output html lines
 output_html_lines = [
@@ -99,12 +167,14 @@ output_html_lines = [
 ]
 
 # add information to the html from the processed data
-for (span_info, text_size) in div_text_info:
+for (span_info, text_size, font_family) in div_text_info:
     output_html_lines.append('<div style="' + FONT_SIZE_STR + ':' + text_size + '">')
-    for text, font in span_info:
-        output_html_lines.append('<p style="' + FONT_FAMILY_STR + ':' + font + '">')
+    output_html_lines.append('<p style="' + FONT_FAMILY_STR + ':' + font_family + '">')
+    for text, font_style in span_info:
+        output_html_lines.append(get_font_style_delimeter(font_style, True))
         output_html_lines.append(text)
-        output_html_lines.append('</p>')
+        output_html_lines.append(get_font_style_delimeter(font_style, False))
+    output_html_lines.append('</p>')
     output_html_lines.append('</div>')
 
 # finish the output html lines
