a786b56f514afd7ef02461e89bcfaae14834fdcd
Reagan <reagan.kelley@wsu.edu>
2022-11-23 22:18:29 -0800

    refactored tests folder, added skeleton code for test_tag_tree.py

---
+++ tests/test_tag_tree.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestTagTree(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
+    def test_default1(self):
+        self.assertTrue(True)
+    
+    def test_default2(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
