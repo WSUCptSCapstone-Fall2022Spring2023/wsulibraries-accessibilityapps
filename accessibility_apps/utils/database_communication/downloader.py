eef6e8018a873f0569487452754bad6cceaa2b31
Trent1048 <trentbultsma@gmail.com>
2023-01-19 16:19:00 -0800

    add way to continually harvest document identifiers

--- accessibility_apps/utils/database_communication/downloader.py
+++ accessibility_apps/utils/database_communication/downloader.py
@@ -9,33 +9,72 @@
 import requests
 from xml.etree import ElementTree
 
-response = requests.get(
-    url="https://na01.alma.exlibrisgroup.com/view/oai/01ALLIANCE_WSU/request",
-    params=
-        {
+REPOSITORY_URL = "https://na01.alma.exlibrisgroup.com/view/oai/01ALLIANCE_WSU/request"
+OAI_STANDARD_PREFIX = ".//{http://www.openarchives.org/OAI/2.0/}"
+
+class DocumentDownloader():
+    """Provides a stream of documents from the WSU research exchange repository."""
+
+    def __init__(self):
+        """Initializes the stream of documents."""
+        self.resumption_token = None
+        self.identifiers = self._get_identifier_batch(initial=True)
+
+    def _get_identifier_batch(self, initial=False) -> list[str]:
+        """Gets the next batch of identifiers."""
+
+        # setup the request paramaters
+        requestParams = {
             "verb":"ListIdentifiers",
             "metadataPrefix":"esploro",
             "set":"OA_Yes"
         }
-    )
+        if not initial:
 
-data = ElementTree.fromstring(response.content)
-for id in data.findall(".//{http://www.openarchives.org/OAI/2.0/}identifier"):
-    print(id.text)
+            # a subsequent batch of identifiers requires a resumption 
+            # token so return an empty list if there is none because 
+            # there will be no identifiers anyway as a result
+            if self.resumption_token is None:
+                return []
 
-resumptionToken = data.find(".//{http://www.openarchives.org/OAI/2.0/}resumptionToken").text
-
-# could loop this more times to get more identifiers
-for _ in range(3):
-    response = requests.get(
-        url="https://na01.alma.exlibrisgroup.com/view/oai/01ALLIANCE_WSU/request",
-        params=
-            {
+            requestParams = {
                 "verb":"ListIdentifiers",
-                "resumptionToken":resumptionToken
+                "resumptionToken":self.resumption_token
             }
-        )
 
-    data = ElementTree.fromstring(response.content)
-    for id in data.findall(".//{http://www.openarchives.org/OAI/2.0/}identifier"):
-        print(id.text)
+        # get the data from the repository
+        response = requests.get(url=REPOSITORY_URL, params=requestParams)
+        data = ElementTree.fromstring(response.content)
+
+        # update the resumption token for getting the next batch
+        try:
+            self.resumption_token = data.find(OAI_STANDARD_PREFIX + "resumptionToken").text
+        except:
+            self.resumption_token = None
+
+        # create a list of the identifiers from the data
+        identifiers = []
+        for id in data.findall(OAI_STANDARD_PREFIX + "identifier"):
+            identifiers.append(id.text)
+
+        return identifiers
+
+    def get_next_document(self):
+        """Gets the next document from the repository."""
+
+        # refill the identifier list if it is empty
+        if len(self.identifiers) == 0:
+            self.identifiers = self._get_identifier_batch()
+        
+        # at this point, we have run out of documents
+        if len(self.identifiers) == 0:
+            return None
+
+        # TODO download the document with the given identifier
+        # but for now just return the identifier
+        return self.identifiers.pop()
+
+if __name__ == "__main__":
+    downloader = DocumentDownloader()
+    for _ in range(15):
+        print(downloader.get_next_document())
