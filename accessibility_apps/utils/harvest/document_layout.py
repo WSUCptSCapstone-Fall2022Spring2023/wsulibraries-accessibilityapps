22ed30e6a8455fc1a737fe104f0db63f6569b153
buddicus <reagan.kelley@wsu.edu>
2023-01-31 13:30:06 -0800

    got trained model to work on sample pdf.

--- accessibility_apps/utils/harvest/document_layout.py
+++ accessibility_apps/utils/harvest/document_layout.py
@@ -15,8 +15,18 @@ def document_layout(pdf_name : str):
         raise FileNotFoundError("Pdf File does not exist: {}".format(pdf_file))
    
     img = np.asarray(pdf2image.convert_from_path(pdf_file)[0])
-    print(type(img))
-    pdf2image.convert_from_path(pdf_file)[0].save('test.jpg', 'JPEG')
+    
+    #print(type(img))
+    #pdf2image.convert_from_path(pdf_file)[0].save('test.jpg', 'JPEG')
+
+    model = lp.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config',
+                                 extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5],
+                                 label_map={0: "Text", 1: "Title", 2: "List", 3:"Table", 4:"Figure"})
+
+    layout_result = model.detect(img)
+
+    for result in layout_result:
+        print(result)
 
 def main():
     document_layout('example.pdf')
