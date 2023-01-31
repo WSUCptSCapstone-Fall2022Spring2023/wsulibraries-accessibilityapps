f2255e5de5acc969c06cf411a0da853407bd5cb1
marisaloyd3 <marisa.loyd@wsu.edu>
2023-01-31 15:33:41 -0800

    pdf_exporter.js working on windows

--- accessibility_apps/utils/export/pdf_exporter.js
+++ accessibility_apps/utils/export/pdf_exporter.js
@@ -1,26 +1,26 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const os = require('node:os');
-const path = require('path');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const os = require('node:os');
+const path = require('path');
 
-(async () => {
-    const browser = await puppeteer.launch();
+(async () => {
+    const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions'], args: ['--no-sandbox', '--disable-setuid-sandbox'],});
 
-    const page = await browser.newPage();
+    const page = await browser.newPage();
 
-    const web_url = path.join(__dirname, '..','..','..','data','output','example.html');
-    const html = fs.readFileSync(web_url, 'utf-8');
-    await page.setContent(html, {waitUntil: 'domcontentloaded'});
+    const web_url = path.join(__dirname, '..','..','..','data','output','example.html');
+    const html = fs.readFileSync(web_url, 'utf-8');
+    await page.setContent(html, {waitUntil: 'domcontentloaded'});
 
-    await page.emulateMediaType('screen');
+    await page.emulateMediaType('screen');
 
-    const pdf = await page.pdf({
-        path : path.join(__dirname, '..','..','..','data', 'output', 'exampleOutput.pdf'),
-        margin: { top: '100px', right: '50px', bottom: '100px', left:'50px'},
-        printBackground: true,
-        format: 'A4',
+    const pdf = await page.pdf({
+        path : path.join(__dirname, '..','..','..','data', 'output', 'exampleOutput.pdf'),
+        margin: { top: '100px', right: '50px', bottom: '100px', left:'50px'},
+        printBackground: true,
+        format: 'A4',
     });
 
-    await browser.close();
+    await browser.close();
 
 })();
