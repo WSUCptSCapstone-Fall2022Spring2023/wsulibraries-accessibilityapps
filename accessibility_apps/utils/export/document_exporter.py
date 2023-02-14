0061089838a07c25476790f1781d78e1d4c78df0
marisaloyd3 <marisa.loyd@wsu.edu>
2023-02-14 12:12:34 -0800

    exporter python function working

--- accessibility_apps/utils/export/document_exporter.py
+++ accessibility_apps/utils/export/document_exporter.py
@@ -2,10 +2,10 @@
 
 """ Creates a pdf from a html link
 """
-
 import linecache
 from fpdf import FPDF
 import subprocess
+import sys
 
 # Last Edit By: Reagan Kelley
 # * Edit Details: Current code is from Marisa
@@ -18,34 +18,9 @@ def export_document(filename):
     Args:
         filename (string): File address location where the PDF will be sent.
     """
-    print("export_document() -> pass.")
+    print(filename)
     p = subprocess.Popen(['node', 'pdf_exporter.js', 'example.html'], stdout=subprocess.PIPE)
+    print("export_document() -> pass.")
     return
     # TODO: Implement solution with Document class attributes.
-    pdf = FPDF()
-    
-    pdf.add_page()
-    
-    # heading font size = 27
-    pdf.set_font("Arial", size = 27)
-
-    #get heading of pdf 
-    line = linecache.getline(filename,1)
-
-    #iterative variable for lines 2 -> end of pdf
-    n = 2
-
-    #create pdf cell aligned in center
-    pdf.cell(200, 10, txt = line, ln = 1, align = 'C')
-    with open(filename) as f:
-        for line in f:
-            #body font size = 12
-            pdf.set_font("Arial", size = 12)
-            line = linecache.getline(filename,n)
-            #next line 
-            n = n+1
-            #add current line to pdf aligned to the left
-            pdf.cell(200, 10, txt = line, ln = 1, align = 'L')
-
-    pdf.output("test.pdf")
 
