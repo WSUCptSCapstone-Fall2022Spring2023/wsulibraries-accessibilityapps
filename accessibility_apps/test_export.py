48acb6ad892af0faf9d0779480ec87c688f347e7
marisaloyd <marisa.loyd@wsu.edu>
2023-04-25 12:36:40 -0700

    update test file

--- accessibility_apps/test_export.py
+++ accessibility_apps/test_export.py
@@ -11,10 +11,8 @@ from utils.accessible_document import AccessibleDocument
 output_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+ "/data/output"
 input_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+ "/data/input"
 
-DATA_INPUT_DIR = Path(os.path.realpath(os.path.dirname(__file__))).parent.absolute().joinpath("data").joinpath("input")
-
 example_pdf = input_folder+"/example.pdf"
 doc1 = AccessibleDocument(str(example_pdf)) # retrieved from .../data/input/example.pdf
-#export_to_html(doc1, output_folder+"/example.html")
+export_to_html(doc1, output_folder+"/example.html")
 
 export_document_to_pdf(output_folder + "/example.html", output_folder + "/example.pdf")
